dc562fa1ea1f56d3b824082dd26fc9070ef4429e
korneevella-boop <korneev.ella@gmail.com>
2026-02-26 18:53:34 +0200

    MAIN ROOM

--- room1code.js
+++ room1code.js
@@ -2,13 +2,20 @@ const bookBtn = document.getElementById("bookButton");
 const popup = document.getElementById("bookPopup");
 const closeBtn = document.getElementById("closePopup");
 
-bookBtn.addEventListener("click", () => popup.classList.add("show"));
-closeBtn.addEventListener("click", () => popup.classList.remove("show"));
+bookBtn.addEventListener("click", () => {
+  popup.classList.add("show");
+});
 
+closeBtn.addEventListener("click", () => {
+  popup.classList.remove("show");
+});
+
+// סגירה בלחיצה על השחור מסביב
 popup.addEventListener("click", (e) => {
   if (e.target === popup) popup.classList.remove("show");
 });
 
+// סגירה עם ESC
 window.addEventListener("keydown", (e) => {
   if (e.key === "Escape") popup.classList.remove("show");
 });
