a131c388ba146f2a69c0efc9537109935af31944
korneevella-boop <korneev.ella@gmail.com>
2026-03-16 18:25:29 +0200

    BookNum.

---
+++ Js/BookNum.js
@@ -0,0 +1,8 @@
+// Room 11 code
+function checkStar(num) {
+    console.log("לחצת על כוכב מספר: " + num);
+    alert("גילית את רמז מספר " + num + "!");
+    
+    // כאן בהמשך תוכלי להוסיף תנאים, למשל:
+    // if(num === 3) { ... }
+}
