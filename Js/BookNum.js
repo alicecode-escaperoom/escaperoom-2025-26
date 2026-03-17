fb3e3edf30291dcbec44436b8fd414b45ed3f6b6
korneevella-boop <korneev.ella@gmail.com>
2026-03-17 18:27:03 +0200

    ss

--- Js/BookNum.js
+++ Js/BookNum.js
@@ -5,10 +5,69 @@ backArrow.addEventListener("click", () => {
     window.location.href = "mainRoom.html";
 });
 
-function checkStar(num) {
-    console.log("לחצת על כוכב מספר: " + num);
-    alert("גילית את רמז מספר " + num + "!");
+let currentStep = 0;
+const expectedSequence = [2, 5, 6, 3]; 
+let collectedCode = [];
+
+const puzzleData = {
+    2: {
+        img: "../pictures/open_book2.png", 
+        hint: "הינשוף שומר על החוכמה. ספרו רק מסגרות שלמות.",
+        digit: 3
+    },
+    5: {
+        img: "../pictures/open_book5.png",
+        hint: "העבר אהב קישוטים. ספרו רק סמלים עצמאיים.",
+        digit: 6
+    },
+    6: {
+        img: "../pictures/open_book6.png",
+        hint: "מה שלא מתאים לאחרים חייב להיספר.",
+        digit: 1
+    },
+    3: {
+        img: "../pictures/open_book3.png",
+        hint: "לא ההתחלה, לא הסוף. רק מה שבמרכז.",
+        digit: 2
+    }
+};
+
+function checkStar(bookNum) {
+    const expectedBook = expectedSequence[currentStep];
+
+    if (bookNum === expectedBook) {
+        openPuzzle(bookNum);
+    } else {
+        alert("שום דבר מעניין כאן כרגע...");
+    }
+}
+
+function openPuzzle(num) {
+    const modal = document.getElementById("puzzleModal");
+    const pimg = document.getElementById("puzzleImage");
+    const hText = document.getElementById("hintText");
+
+    pimg.src = puzzleData[num].img;
+    hText.innerText = puzzleData[num].hint;
+    hText.classList.add("hidden"); 
+    modal.style.display = "block";
+
+    if (collectedCode.length === currentStep) {
+        collectedCode.push(puzzleData[num].digit);
+        console.log("ספרות שנאספו: " + collectedCode.join(""));
+    }
+}
+
+function closePuzzle() {
+    document.getElementById("puzzleModal").style.display = "none";
+    // אם פתחנו את החידה הנכונה, נתקדם לשלב הבא ברצף
+    currentStep++; 
     
-    // כאן בהמשך תוכלי להוסיף תנאים, למשל:
-    // if(num === 3) { ... }
+    if (currentStep === expectedSequence.length) {
+        alert("אספת את כל הרמזים! עכשיו נסי לפתוח את המנעול.");
+    }
+}
+
+function showHint() {
+    document.getElementById("hintText").classList.toggle("hidden");
 }
